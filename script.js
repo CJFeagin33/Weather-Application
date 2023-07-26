1d47f900e9c8b232db761ba1dd5c3cec9889d899
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-07-26 11:20:08 -0700

    able to pull temp, wind and humidity for san diego

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-var lat = 44.34;
-var lon = 10.99;
+var currentCity = document.querySelector('.currentCity')
+var currentTemp = document.querySelector('.Temperature')
+var currentWind = document.querySelector('.Wind')
+var currentHumidity = document.querySelector('.Humidity')
+
+var cityName = 'San Diego'
 
 var Weather_API_Key = "7cf47c1dd3cac3c59e200954bea4d663";
-var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${Weather_API_Key}`
+var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${Weather_API_Key}`
 
 
-
-var responseText = document.querySelector(".card")
-
-function getApi(url){
-    console.log(url)
-    fetch(url)
-        .then(function (response){
-            console.log(response);
-            if (response.status === 200) {
-                responseText.textContent = response.status;
-            }
-            return response.json();
-    });
-}
-
-getApi(url)
+fetch(url).then(function (response) {
+    return response.json()
+}).then(function (data) {
+    console.log(data)
+    currentCity.textContent = data.city.name + ' ' + data.list[0].dt_txt
+    currentTemp.textContent = 'Temperature: ' + Math.round(((((data.list[0].main.temp - 273)*9)/5)+32)) + ' °F'
+    currentWind.textContent = 'Wind: ' + data.list[0].wind.speed + ' meters/sec'
+    currentHumidity.textContent= 'Humidity: ' + data.list[0].main.humidity + '%'
+    return
+})

c18eaae7949847ad41a21880651705cbb89486af
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-07-26 13:22:10 -0700

    added search functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Input and Search BTN Variables:
+
+var searchBar = document.querySelector('.citySearch')
+var searchBtn = document.querySelector('.searchBtn')
+
 // Current Day Forcast Variables:
 
 var currentCity = document.querySelector('.currentCity')
@@ -32,50 +37,58 @@ var Temp5 = document.querySelector('.Temp5')
 var Wind5 = document.querySelector('.Wind5')
 var Humidity5 = document.querySelector('.Humidity5')
 
-var cityName = 'San Diego'
+// API Information
+var getWeather = function () {
+    var cityName = searchBar.value
+    var Weather_API_Key = "7cf47c1dd3cac3c59e200954bea4d663";
+    var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${Weather_API_Key}`
 
-var Weather_API_Key = "7cf47c1dd3cac3c59e200954bea4d663";
-var url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${Weather_API_Key}`
+    if (!cityName) {
+        alert('Error! Make sure to insert a City in the Search Bar.')
+        return
+    }
 
+    fetch(url).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        console.log(data)
 
-fetch(url).then(function (response) {
-    return response.json()
-}).then(function (data) {
-    console.log(data)
+        // Current Day Weather Forcast:
 
-    // Current Day Weather Forcast:
+        currentCity.textContent = data.city.name + ' ' + data.list[0].dt_txt
+        currentTemp.textContent = 'Temp: ' + Math.round(((((data.list[0].main.temp - 273)*9)/5)+32)) + ' °F'
+        currentWind.textContent = 'Wind: ' + Math.round((data.list[0].wind.speed)*2.237) + ' MPH'
+        currentHumidity.textContent= 'Humidity: ' + data.list[0].main.humidity + ' %'
 
-    currentCity.textContent = data.city.name + ' ' + data.list[0].dt_txt
-    currentTemp.textContent = 'Temp: ' + Math.round(((((data.list[0].main.temp - 273)*9)/5)+32)) + ' °F'
-    currentWind.textContent = 'Wind: ' + Math.round((data.list[0].wind.speed)*2.237) + ' MPH'
-    currentHumidity.textContent= 'Humidity: ' + data.list[0].main.humidity + ' %'
+        // 5 Day Weather Forcast:
 
-    // 5 Day Weather Forcast:
+        Date1.textContent = data.list[5].dt_txt
+        Temp1.textContent = 'Temp: ' + Math.round(((((data.list[5].main.temp - 273)*9)/5)+32)) + ' °F'
+        Wind1.textContent = 'Wind: ' + Math.round((data.list[5].wind.speed)*2.237) + ' MPH'
+        Humidity1.textContent = 'Humidity: ' + data.list[5].main.humidity + ' %'
 
-    Date1.textContent = data.list[5].dt_txt
-    Temp1.textContent = 'Temp: ' + Math.round(((((data.list[5].main.temp - 273)*9)/5)+32)) + ' °F'
-    Wind1.textContent = 'Wind: ' + Math.round((data.list[5].wind.speed)*2.237) + ' MPH'
-    Humidity1.textContent = 'Humidity: ' + data.list[5].main.humidity + ' %'
+        Date2.textContent = data.list[13].dt_txt
+        Temp2.textContent = 'Temp: ' + Math.round(((((data.list[13].main.temp - 273)*9)/5)+32)) + ' °F'
+        Wind2.textContent = 'Wind: ' + Math.round((data.list[13].wind.speed)*2.237) + ' MPH'
+        Humidity2.textContent = 'Humidity: ' + data.list[13].main.humidity + ' %'
 
-    Date2.textContent = data.list[13].dt_txt
-    Temp2.textContent = 'Temp: ' + Math.round(((((data.list[13].main.temp - 273)*9)/5)+32)) + ' °F'
-    Wind2.textContent = 'Wind: ' + Math.round((data.list[13].wind.speed)*2.237) + ' MPH'
-    Humidity2.textContent = 'Humidity: ' + data.list[13].main.humidity + ' %'
+        Date3.textContent = data.list[21].dt_txt
+        Temp3.textContent = 'Temp: ' + Math.round(((((data.list[21].main.temp - 273)*9)/5)+32)) + ' °F'
+        Wind3.textContent = 'Wind: ' + Math.round((data.list[21].wind.speed)*2.237) + ' MPH'
+        Humidity3.textContent = 'Humidity: ' + data.list[21].main.humidity + ' %'
 
-    Date3.textContent = data.list[21].dt_txt
-    Temp3.textContent = 'Temp: ' + Math.round(((((data.list[21].main.temp - 273)*9)/5)+32)) + ' °F'
-    Wind3.textContent = 'Wind: ' + Math.round((data.list[21].wind.speed)*2.237) + ' MPH'
-    Humidity3.textContent = 'Humidity: ' + data.list[21].main.humidity + ' %'
+        Date4.textContent = data.list[29].dt_txt
+        Temp4.textContent = 'Temp: ' + Math.round(((((data.list[29].main.temp - 273)*9)/5)+32)) + ' °F'
+        Wind4.textContent = 'Wind: ' + Math.round((data.list[29].wind.speed)*2.237) + ' MPH'
+        Humidity4.textContent = 'Humidity: ' + data.list[29].main.humidity + ' %'
 
-    Date4.textContent = data.list[29].dt_txt
-    Temp4.textContent = 'Temp: ' + Math.round(((((data.list[29].main.temp - 273)*9)/5)+32)) + ' °F'
-    Wind4.textContent = 'Wind: ' + Math.round((data.list[29].wind.speed)*2.237) + ' MPH'
-    Humidity4.textContent = 'Humidity: ' + data.list[29].main.humidity + ' %'
+        Date5.textContent = data.list[37].dt_txt
+        Temp5.textContent = 'Temp: ' + Math.round(((((data.list[37].main.temp - 273)*9)/5)+32)) + ' °F'
+        Wind5.textContent = 'Wind: ' + Math.round((data.list[37].wind.speed)*2.237) + ' MPH'
+        Humidity5.textContent = 'Humidity: ' + data.list[37].main.humidity + ' %'
 
-    Date5.textContent = data.list[37].dt_txt
-    Temp5.textContent = 'Temp: ' + Math.round(((((data.list[37].main.temp - 273)*9)/5)+32)) + ' °F'
-    Wind5.textContent = 'Wind: ' + Math.round((data.list[37].wind.speed)*2.237) + ' MPH'
-    Humidity5.textContent = 'Humidity: ' + data.list[37].main.humidity + ' %'
-    
-    return
-})
+        return
+    })
+}
+
+searchBtn.addEventListener('click', getWeather)
